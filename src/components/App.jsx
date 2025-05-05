e0e3caf35533f744c4c385a234b63490d06f018e
GordonVL <ladaniv123@gmail.com>
2025-05-05 23:49:25 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,10 @@ import Profile from "./task-1/Profile";
 import userData from "./task-1/user-data.json";
 import Friends from "./task-2/Friend";
 import friendData from "./task-2/friends.json";
-import Transactions from "./task-3/transactions";
+import Transactions from "./task-3/Transactions";
 import transactionsList from "./task-3/transactions.json";
 
 export default function App() {
-  console.log(friendData)
   return (
     <div>
       <h2>Task 1. Profile</h2>
